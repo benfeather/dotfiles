280242fd784309a00c1b8b5a295a53df3637051f
Ben Feather <contact@benfeather.dev>
2025-06-01 16:43:57 +1200

    WIP

--- nix/hosts/linux/fennec.nix
+++ nix/hosts/linux/fennec.nix
@@ -4,8 +4,34 @@
   ...
 }:
 {
-  hardware.nvidia = {
-    enable = true;
-    modesetting.enable = true;
-  }
+  environment.systemPackages = with pkgs; [
+    bottles
+    gnome-shell-extension-dash-to-dock
+    heroic
+    lutris
+    mangohud
+    protonup-qt
+  ];
+
+  hardware = {
+    nvidia = {
+      enable = true;
+      modesetting.enable = true;
+    };
+
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32bit = true;
+    };
+  };
+
+  programs = {
+    gamemode.enable = true;
+
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+  };
 }
